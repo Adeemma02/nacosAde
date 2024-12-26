296bd3827f5ab2bc285cadde59f36c821bc68ecf
Adeemma02 <emm.bolu@gmail.com>
2024-12-27 00:16:35 +0100

    old

--- var.js
+++ var.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.menu-box');
 const navSide = document.querySelector('ul')
+// const hamburger = document.querySelector('.hamburger')
 
 menu.addEventListener('click', ()=>{
     navSide.classList.toggle('active')
+    menu.classList.toggle('active')
 })
